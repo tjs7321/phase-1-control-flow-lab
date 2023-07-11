a8f3ccf691a2e5a17d05ed06614a0d7221f82925
Theodore Smith <tjs7321@gmail.com>
2023-07-10 22:58:38 -0400

    finished

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(i){
+  let response
+  if (i <= 400) {
+    response = 'This one is on me!'
+  }
+  else if (i <= 2000) {
+    response = 'That will be twenty bucks.'
+  }
+  else if (i <= 2500) {
+    response = 'I will gladly take your thirty bucks.'
+  }
+  else if (i > 2500) {
+    response = 'No can do.'
+  }
+  return response
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(c){
+  let response2
+  if (c == "NYC") {
+    response2 = 'Ok, sounds good.'
+  }
+  else {
+    response2 = 'No go.'
+  }
+  return response2
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(t){
+  let response3
+  if (t === 'generous') {
+    response3 = "Thank you so much."
+  }
+  else if (t === 'not as generous') {
+    response3 = "Thank you."
+  }
+  else {
+    response3 = "Bye."
+  }
+  return response3
 }
